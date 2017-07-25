95b08c52e9ca85cca84e5d9a91c2302efb65e7fa
Sarah Charles <sarah.charles007@gmail.com>
2017-07-25 17:51:28 -0400

    added review model

---
+++ server/db/models/review.js
@@ -0,0 +1,21 @@
+const Sequelize = require('sequelize');
+const db = require('../db');
+
+const Review = db.define('review', {
+  status: {
+    type: Sequelize.ENUM,
+    defaultValue: 'pending',
+    values: ['pending', 'approved']
+  },
+  review: {
+    type: Sequelize.TEXT
+  },
+  rating: {
+    type: Sequelize.ENUM,
+    values: ['friendly', 'neutral', 'a little prickly']
+    //Make it a scale from 1-3 that can be averaged out? and translates to these words?
+  }
+});
+
+module.exports = Review;
+
