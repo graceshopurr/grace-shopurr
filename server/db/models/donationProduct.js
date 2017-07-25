0ce6a777b8c4fe6c33fc3da90dedc2edf9a6d26e
India Amos <india@indiamos.com>
2017-07-25 18:29:36 -0400

    Export the DonationProduct model

--- server/db/models/donationProduct.js
+++ server/db/models/donationProduct.js
@@ -2,26 +2,31 @@ const Sequelize = require('sequelize')
 const db = require('../db')
 
 const DonationProduct = db.define('donationProduct', {
-	name : {
-		type: Sequelize.STRING, 
-		unique: true, 
-		allowNull: false
-	}, 
-	description : {
-		type: Sequelize.TEXT
-	}, 
-	price : {
-		type: Sequelize.INTEGER, //price in cents, 
-		defaultValue: 0
-	},  //leaving off 'rating field' -- virtual field derived from associated reviews? 
+    name: {
+      type: Sequelize.STRING,
+      unique: true,
+      allowNull: false
+    },
+    description: {
+      type: Sequelize.TEXT
+    },
+    price: {
+      type: Sequelize.INTEGER, //price in cents,
+      defaultValue: 0
+    }, //leaving off 'rating field' -- virtual field derived from associated reviews?
 
-}, 
+  },
 
-{
-	getterMethods: {
-		dollarPrice: function(this.price){
-			return this.price/100;
-		}
-	}
-}
+  {
+    getterMethods: {
+      dollarPrice: function(this.price) {
+        return this.price / 100;
+      }
+    }
+  }
 )
+
+module.exports = {
+	db,
+	DonationProduct
+}
