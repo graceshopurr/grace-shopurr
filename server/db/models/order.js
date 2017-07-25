9418a68a97fe06ef61a37aacb507599a7f93f3f9
India Amos <india@indiamos.com>
2017-07-25 17:54:02 -0400

    Define order model

---
+++ server/db/models/order.js
@@ -0,0 +1,21 @@
+const Sequelize = require('sequelize');
+const db = require('../db');
+
+const Order = db.define('order', {
+  userId: {
+    type: Sequelize.STRING,
+    defaultValue: null
+  },
+  status: {
+    type: Sequelize.ENUM,
+    values: ['pending','shipped','delivered', 'canceled']
+  },
+  cats: {
+    type: Sequelize.ARRAY,
+    defaultValue: []
+  },
+  donationProducts: {
+    type: Sequelize.ARRAY,
+    defaultValue: []
+  }
+})
