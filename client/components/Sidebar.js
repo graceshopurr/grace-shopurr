dba0b7ae9306dacf191613cfb117499649aec793
Sarah Charles <sarah.charles007@gmail.com>
2017-07-26 17:23:11 -0400

    FEAT(Sidebar): Added sidebar and basic (not great) bootstrap styling

---
+++ client/components/Sidebar.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import {withRouter, Link} from 'react-router-dom';
+
+
+export default class Sidebar extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      inputValue: ''
+    }
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+
+  }
+
+  handleChange (event) {
+    console.log(event.target.value);
+    this.setState({inputValue: event.target.value});
+  }
+
+  handleSubmit (event) {
+    event.preventDefault();
+    console.log(this.state.inputValue);
+    this.setState({inputValue: ''});
+  }
+
+  render(){
+    return (
+      <div id="sidebar" className="sidebar-offcanvas">
+          <div className="col-md-12">
+            <h3>Sidebar</h3>
+            <ul className="nav ">
+              <li className="active"><a href="#">Home</a></li>
+              <li><a href="#">Cats</a></li>
+              <li><a href="#">Donations</a></li>
+
+            </ul>
+            <form onSubmit={this.handleSubmit}>
+              Search: <br />
+              <input
+              onChange={this.handleChange}
+              type="text" name="Search"
+              value = {this.state.inputValue} />
+
+              <button className="btn btn-primary" type="button">GO</button>
+            </form>
+          </div>
+      </div>
+    );
+  }
+
+}
+
+
