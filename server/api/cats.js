2c228f7b36069d2fd663c8e37ced2ab9a1a108fc
Maggy Prajapati <mupraj10@gmail.com>
2017-07-26 14:39:47 -0400

    cat routes are in

--- server/api/cats.js
+++ server/api/cats.js
@@ -4,9 +4,9 @@ module.exports = router;
 
 // /cats - shows all cats!
 // /cats/:id - shows a particular cat
-// /cats/new - creates a cat!
-// /cats/:id/update - updates a cat
-// /cats/:id/delete - deletes a cat
+// POST /cats - creates a cat!
+// PUT /cats/:id - updates a cat
+// DELETE /cats/:id - deletes a cat
 // /cats/:id/addReview ?
 
 // GET /api/cats
@@ -24,22 +24,22 @@ router.get('/:catId', (req, res, next) => {
 })
 
 // POST /api/cats/newCat
-router.post('/newCat', (req, res, next) => {
+router.post('/', (req, res, next) => {
     Cat.create(req.body)
     .then( (cat) => res.json(cat))
     .catch(next);
 })
 
-// PUT /api/cats/:catId/update
-router.put('/:catId/update', (req, res, next) => {
+// PUT /api/cats/:catId
+router.put('/:catId', (req, res, next) => {
     const id = req.params.catId;
     Cat.findById(id)
     .then(cat => cat.update(req.body) )
     .catch(next);
 })
 
-// DELETE /api/cats/:catId/delete
-router.delete('/:catId/delete', (req, res, next) => {
+// DELETE /api/cats/:catId
+router.delete('/:catId', (req, res, next) => {
     const id = req.params.catId;
     Cat.findById(id)
     .then(foundCat => {return foundCat.destroy()} )
