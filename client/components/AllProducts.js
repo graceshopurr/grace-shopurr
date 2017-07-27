202c88062bd1c75d4a8af014c20b37a5cae5d559
Maggy Prajapati <mupraj10@gmail.com>
2017-07-27 18:32:58 -0400

    added missing store

--- client/components/AllProducts.js
+++ client/components/AllProducts.js
@@ -1,47 +1,44 @@
 import React, { Component } from 'react';
-import axios from 'axios';
+import {withRouter, Link} from 'react-router-dom'
+import store from '../store';
+
 
 
 export default class AllProducts extends Component {
 
   constructor () {
     super();
-    this.state = {
-      products: []
-    };
+    this.state = store.getState()
   }
 
-  componentDidMount () {
-    axios.get('/api/products')
-      .then(res => res.data)
-      .then(products => this.setState({ products }));
-  }
+    render() {
 
-  render () {
+        const products = this.state.products;
 
-    const products = this.state.products;
-
-    return (
-        <div>
-            <h3>Support Our Kitties!!!</h3>
-            <div className="row">
-                {
-                    products.map(product => (
-                        <div className="col-xs-4" key={product.id}>
-                            <Link to={`/products/${product.id}`}>
-                                <div className="caption">
-                                    <h5>
-                                        <span>{product.name}</span>
-                                    </h5>
-                                    <small>{product.songs.length} songs</small>
-                                </div>
-                            </Link>
-                        </div>
-                    ))
-                }
+        return (
+            <div>
+                <h3>Support Our Kitties!!!</h3>
+                <div className="row">
+                    {
+                        products.map(product => (
+                            <div className="col-xs-4" key={product.id}>
+                                <Link to={`/products/${product.id}`}>
+                                    <div className="caption">
+                                        <h5>
+                                            <span>{product.name}</span>
+                                        </h5>
+                                        <p>{product.description}</p>
+                                        <span>${product.dollarPrice()}</span>
+                                    </div>
+                                </Link>
+                            </div>
+                        ))
+                    }
+                </div>
             </div>
-        </div>
-    )
-  }
+        )
+    }
 }
-//maybe add description of the page 
+}
+//maybe add description of the page
+//check out getter method working 
