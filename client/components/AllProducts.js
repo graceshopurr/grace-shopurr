ad5f1ebcce72cd1cf9b82ec217110d13f567d00f
India Amos <india@indiamos.com>
2017-07-27 17:59:58 -0400

    FIX: Rename all things donation or donationProduct to just products

--- client/components/AllProducts.js
+++ client/components/AllProducts.js
@@ -2,38 +2,38 @@ import React, { Component } from 'react';
 import axios from 'axios';
 
 
-export default class AllDonationProducts extends Component {
+export default class AllProducts extends Component {
 
   constructor () {
     super();
     this.state = {
-      donations: []
+      products: []
     };
   }
 
   componentDidMount () {
-    axios.get('/api/donations')
+    axios.get('/api/products')
       .then(res => res.data)
-      .then(donations => this.setState({ donations }));
+      .then(products => this.setState({ products }));
   }
 
   render () {
 
-    const donations = this.state.donations;
+    const products = this.state.products;
 
     return (
         <div>
             <h3>Support Our Kitties!!!</h3>
             <div className="row">
                 {
-                    donations.map(donation => (
-                        <div className="col-xs-4" key={donation.id}>
-                            <Link to={`/donations/${donation.id}`}>
+                    products.map(product => (
+                        <div className="col-xs-4" key={product.id}>
+                            <Link to={`/products/${product.id}`}>
                                 <div className="caption">
                                     <h5>
-                                        <span>{donation.name}</span>
+                                        <span>{product.name}</span>
                                     </h5>
-                                    <small>{donation.songs.length} songs</small>
+                                    <small>{product.songs.length} songs</small>
                                 </div>
                             </Link>
                         </div>
--- server/api/index.js
+++ server/api/index.js
@@ -3,7 +3,7 @@ module.exports = router
 
 router.use('/users', require('./users'))
 router.use('/cats', require('./cats'));
-router.use('/donations', require('./donations'));
+router.use('/products', require('./products'));
 
 router.use((req, res, next) => {
   const error = new Error('Not Found')
