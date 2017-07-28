b095e88d67bb2e4d531fcb91f3cfd50ec7d35f81
Maggy Prajapati <mupraj10@gmail.com>
2017-07-28 12:14:39 -0400

    attempting to resolve error issue

--- client/index.js
+++ client/index.js
@@ -11,12 +11,7 @@ import socket from './socket'
 
 ReactDOM.render(
   <Provider store={store}>
-  <Router>
     <Routes />
-    <Route exact path="/" component={Main} />    
-    <Route exact path="/cats" component={AllCats} />        
-    <Route exact path="/cats" component={AllProducts} /> 
-    </Router>
   </Provider>,
   document.getElementById('app')
 )
