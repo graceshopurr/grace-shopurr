b3b6d0d9e9a13a6f42bcf425499686c80d35d563
Leigh Steiner <steinerleigh@gmail.com>
2017-07-29 15:20:46 -0400

    added cart reducer to combinedreducer

--- client/store/index.js
+++ client/store/index.js
@@ -6,8 +6,9 @@ import { composeWithDevTools } from 'redux-devtools-extension';
 
 import cat from './cat'
 import product from './product'
+import cart from './cart'
 
-const reducer = combineReducers({user, cat, product})
+const reducer = combineReducers({user, cat, product, cart})
 const middleware = applyMiddleware(thunkMiddleware, createLogger({collapsed: true}))
 const store = createStore(reducer, composeWithDevTools(middleware));
 
