6e8b2bbc76eefc3fad717ba1859ce48cbc3ce5e7
Sarah Charles <sarah.charles007@gmail.com>
2017-07-29 15:53:18 -0400

    FIX(Components/Store): removed root redirect, was actually uneccessary

--- client/Routes.js
+++ client/Routes.js
@@ -21,17 +21,15 @@ class Routes extends Component {
     this.props.loadInitialData();
   }
 
-
   render () {
 
     const {isLoggedIn} = this.props;
 
     return (
       <Router history={history}>
-        <Root>
+        <Main>
           <Switch>
             {/* Routes placed here are available to all visitors */}
-            <Route exact path="/" component={Main} />
             <Route path="/login" component={Login} />
             <Route path="/signup" component={Signup} />
             <Route exact path ="/products" component = {AllProducts} />
@@ -51,7 +49,7 @@ class Routes extends Component {
             {/* Displays our Login component as a fallback */}
             <Route component= {Login} />
           </Switch>
-        </Root>
+        </Main>
       </Router>
     )
   }
