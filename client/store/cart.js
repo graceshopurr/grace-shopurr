a10a21e26ee1d7cbffdaa8593f9c487e5f8c00e4
Leigh Steiner <steinerleigh@gmail.com>
2017-07-29 15:16:33 -0400

    FEAT(reducer): cart reducer

--- client/store/cart.js
+++ client/store/cart.js
@@ -17,7 +17,8 @@ const CANCEL_ORDER = 'CANCEL_ORDER';
 const  initialState = {
 	addedProductIds: [], //which products are in the order at all
 	quantityById: {}, //how many of each product are in the order
-	addedCatIds: [] //which cats are in the order (only one of each cat no need for quantity)
+	addedCatIds: [],  //which cats are in the order (only one of each cat no need for quantity)
+	status: "cart" //all open carts start as 'cart' status
 }
 
 //action creators 
@@ -41,12 +42,17 @@ export default function (state = initialState, action){
 		case REMOVE_CAT_FROM_CART
 			return Object.assign({}, state, {addedCatIds : addedCatIds.filter( cat => cat.id !== action.catId)})
 		case ADD_PRODUCT_TO_CART
-			return Object.assign({}, state, {})
+			return Object.assign({}, state, {addedProductIds: [...state.addedProductIds, action.productId], quantityById:{...state.quantityById, action.productId: 1}})
 		case INCREMENT_PRODUCT_QUANTITY
+			return Object.assign({},state, {quantityById: {...state.quantityById, action.productId: action.productId++}})
 		case DECREMENT_PRODUCT_QUANTITY
+			return Object.assign({},state, {quantityById: {...state.quantityById, action.productId: action.productId--}}
 		case REMOVE_PRODUCT_FROM_CART
+			return Object.assign({}, state, {addedProductIds : addedProductIds.filter( product => product.id !== action.productId), quantityById: {...state.quantityById, action.productId: 0}})
 		case SUBMIT_ORDER
+			return Object.assign({}, state, { status: 'processing'})
 		case CANCEL_ORDER
+			return Object.assign({}, state, {status: 'canceled'})
 		default:
 			return state
 	}
