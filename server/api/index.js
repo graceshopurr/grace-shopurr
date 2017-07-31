e6ec82c300165986688d95cc66cd513851170827
Maggy Prajapati <mupraj10@gmail.com>
2017-07-31 18:07:04 -0400

    TEST(orderroutes): Adding all order routes tests

--- server/api/index.js
+++ server/api/index.js
@@ -4,6 +4,7 @@ module.exports = router
 router.use('/users', require('./users'));
 router.use('/cats', require('./cats'));
 router.use('/products', require('./products'));
+router.use('/orders', require('./orders'));
 
 
 router.use((req, res, next) => {
