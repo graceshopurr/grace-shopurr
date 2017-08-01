9dedb03fd31f5b1f8c3a9c9132734c36c08bb243
Sarah Charles <sarah.charles007@gmail.com>
2017-08-01 17:43:05 -0400

    FEAT(AllCats/AllProducts): filtering and sorting for cats and products

--- client/components/AllProducts.js
+++ client/components/AllProducts.js
@@ -6,10 +6,27 @@ class AllProducts extends Component {
 
   constructor (props) {
     super(props);
+    this.state = {
+      filter: 'none'
+    };
+    this.handleFilter = this.handleFilter.bind(this);
+    this.compare = this.compare.bind(this);
+  }
+
+  handleFilter (event) {
+    this.setState({filter: event.target.value});
+  }
+
+  compare (a, b) {
+    return a.price - b.price;
   }
 
   render() {
-    const products = this.props.product.productList;
+    // const products = this.props.product.productList;
+    let products = this.props.product.productList;
+    if (this.state.filter === 'low') products = products.sort(this.compare);
+    if (this.state.filter === 'high') products = products.sort(this.compare).reverse();
+
     return (
       <div className="AllProducts">
         <h3>Support Our Kitties!!!</h3>
@@ -17,6 +34,10 @@ class AllProducts extends Component {
         <p>
           Want to show love for our cats, even if you can’t bring one of them home with you? You can show your support by buying much-needed supplies for our shelter. This way, you know exactly how your donations are helping our Kitties.
         </p>
+        &nbsp; Sort: &nbsp;
+        <button type="button" className='btn btn-primary' value="low" onClick={this.handleFilter}>Price: Low to High</button> &nbsp;
+        <button type="button" className='btn btn-primary' value="high" onClick={this.handleFilter}>Price: High to Low</button> &nbsp;
+        <hr />
         <div className="row">
           {
             products.map(product => (
